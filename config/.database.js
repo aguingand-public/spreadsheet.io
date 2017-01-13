056d6444de6a4176c793fd7b9b024a50049aea68
aguingand-public <antoine.guingand@etu.unistra.fr>
2017-01-13 14:53:35 +0100

    Harmonisation configuration bdd

--- config/.database.js
+++ config/.database.js
@@ -1,8 +1,8 @@
 module.exports = {
     'connection': {
-        'host': '130.79.158.79',
-        'user': 'dweb3',
-        'password': 'rthR9k7JR58RvuRY',
+        'host': 'localhost',
+        'user': 'root',
+        'password': '',
         'database': 'dweb3'
     },
     'users_table': 'users'
