eb1fb9a4893262731839377c7ddc71c81a5301d8
aguingand-public <antoine.guingand@etu.unistra.fr>
2017-01-13 12:08:26 +0100

    Ajout du gitignore

--- public/js/spreadsheet.js
+++ public/js/spreadsheet.js
@@ -1,11 +1,14 @@
-var data = [
+/*var data = [
   ["", "Ford", "Volvo", "Toyota", "Honda"],
   ["2016", 10, 11, 12, 13],
   ["2017", 20, 11, 14, 13],
   ["2018", 30, 15, 12, 13]
-];
+];*/
+var data = new Array(100);
+for(var i=0;i<100;i++)
+  data = new Array(100);
 $(function() {
-  var container = $('.sheet-container')[0];
+  var container = $('.sheet-container').get(0);
   console.log(container);
   var hot = new Handsontable(container, {
     data: data,
